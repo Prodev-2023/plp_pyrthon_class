df8b0ef3ec4e80752d64e35ab7d59164e4450387
prodev-2023 <protech2023.32@gmail.com>
2024-02-23 15:24:16 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,7 @@
+#plp class challenge
+
+name = input("please put in your Name:");
+age = int(input("please how old are you ?:"));
+location =input("please put in your location:");
+
+print(f"we believe this is your name: {name}, and also your age: {age}yrs, and your location: {location}. thank you {name}! for submitting your details")
